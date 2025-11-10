0490335c3cf7be9e84462ffc226afbee3a09d463
Pivok <pivoc@protonmail.com>
2025-11-10 16:58:32 +0100

    Openssh

--- hosts/vm-dev/configuration.nix
+++ hosts/vm-dev/configuration.nix
@@ -84,6 +84,8 @@
 
   environment.systemPackages = [ ];
 
+  services.openssh.enable = true;
+
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
