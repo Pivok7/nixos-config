93a0319446a7b8d38e0380862cbaa8d5df402a4c
Pivok <pivoc@protonmail.com>
2026-01-07 17:18:00 +0100

    Zathura clipboard

--- modules/home/media/zathura.nix
+++ modules/home/media/zathura.nix
@@ -7,19 +7,29 @@
 
 let
   cfg = config.modHomeMedia.zathura;
+
+  zathuraConfig = ''
+    set selection-clipboard clipboard
+  '';
 in
 {
   options.modHomeMedia.zathura = {
     enable = lib.mkEnableOption "Setup zathura";
     defaultPdfViewer = lib.mkEnableOption "Set as default pdf viewer";
+
+    clipboard = lib.mkOption {
+      type = lib.types.str;
+      default = "clipboard";
+      description = "Set font selection-clipboard option";
+    };
   };
 
   config = lib.mkIf (cfg.enable) {
     home.packages = [
       pkgs.zathura
     ];
-    modHomeMedia.mime.pdf.default = (
-      if cfg.defaultPdfViewer then "org.pwmt.zathura.desktop" else ""
-    );
+    modHomeMedia.mime.pdf.default = (if cfg.defaultPdfViewer then "org.pwmt.zathura.desktop" else "");
+
+    home.file.".config/zathura/zathurarc".text = lib.trim zathuraConfig;
   };
 }
