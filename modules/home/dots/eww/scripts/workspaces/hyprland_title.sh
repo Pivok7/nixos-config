6e49501f42ba9c36a7bec6de1b1ff97ef11c4d40
Pivok <pivoc@protonmail.com>
2026-01-20 21:55:20 +0100

    Fix eww title

--- modules/home/dots/eww/scripts/workspaces/hyprland_title.sh
+++ modules/home/dots/eww/scripts/workspaces/hyprland_title.sh
@@ -1,6 +1,5 @@
-
 title() {
-    hyprctl activewindow | grep "title: " | awk '{print $NF}'
+    hyprctl activewindow | grep "title: " | sed 's/^[^:]*: *//'
 }
 
 HYPRLAND_SIGNATURE_ACTUAL=$(ls -td /run/user/1000/hypr/*/ | head -n1 | xargs basename)
@@ -13,4 +12,3 @@ socat -U - UNIX-CONNECT:"$SOCKET" 2>&1 | while read -r line; do
 	;;
     esac
 done
-
