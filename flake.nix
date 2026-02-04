affb53c913e3eee050417401417e187f4eb20697
Pivok <pivoc@protonmail.com>
2026-02-04 16:22:59 +0100

    Shorten nixpkgs url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
 
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=v0.6.0";
