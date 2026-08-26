d68719ed55fbc8b5a9eeb3b46f0f7fe199b5c095
Pivok <pivoc@protonmail.com>
2026-08-26 17:10:47 +0200

    Nvim update tree-sitter

--- modules/home/dots/nvim/lua/plugins/treesitter.lua
+++ modules/home/dots/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,42 @@
 vim.pack.add({ "https://github.com/nvim-treesitter/nvim-treesitter" })
 
+vim.api.nvim_create_autocmd("User", {
+    pattern = "TSUpdate",
+    callback = function()
+        require("nvim-treesitter.parsers").rocq = {
+            install_info = {
+                url = "https://github.com/aruzdh/tree-sitter-rocq",
+		queries = "queries",
+            },
+        }
+
+        require("nvim-treesitter.parsers").ziggy = {
+            install_info = {
+                url = "https://github.com/kristoff-it/ziggy",
+                location = "tree-sitter-ziggy",
+                queries = "tree-sitter-ziggy/queries",
+            },
+        }
+
+        require("nvim-treesitter.parsers").ziggy_schema = {
+            install_info = {
+                url = "https://github.com/kristoff-it/ziggy",
+                location = "tree-sitter-ziggy-schema",
+                queries = "tree-sitter-ziggy-schema/queries",
+            },
+        }
+    end,
+})
+
 vim.treesitter.language.register("rocq", "coq")
+vim.treesitter.language.register("ziggy_schema", "ziggy-schema")
+
+vim.filetype.add({
+    extension = {
+        ziggy = "ziggy",
+        ["ziggy-schema"] = "ziggy_schema",
+    },
+})
 
 require("nvim-treesitter").install({
     "bash",
@@ -27,6 +63,9 @@ require("nvim-treesitter").install({
     "typst",
     "yaml",
     "zig",
+    "rocq",
+    "ziggy",
+    "ziggy_schema",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
@@ -56,6 +95,8 @@ vim.api.nvim_create_autocmd("FileType", {
         "vim",
         "yaml",
         "zig",
+        "ziggy",
+        "ziggy_schema",
         "coq",
     },
     callback = function()
